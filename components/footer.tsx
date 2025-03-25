2d731ef2e0ea4397b5f3cf965af9f38683153655
Pavlo Myrskyi <pavlo.myrskyi@games>
2025-03-25 13:38:48 +0100

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+"use client";
+
+import React, {useState, useEffect} from "react";
 
 export default function Footer() {
-  return (
+    const [year, setYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        setYear(new Date().getFullYear());
+    }, []);
+
+    return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
+        &copy; {year} Pavlo. All rights reserved.
       </small>
-      <p className="text-xs">
-        <span className="font-semibold">About this website:</span> built with
-        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
-        Framer Motion, React Email & Resend, Vercel hosting.
-      </p>
     </footer>
   );
 }
